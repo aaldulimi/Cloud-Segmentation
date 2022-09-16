d5ccc1f9dcb49bde388a703ade8c8bfa055fc43c
Ahmed <ahmedamedo22@gmail.com>
2022-09-17 01:06:03 +1000

    implemented the u transformations

--- cloud_seg/model.py
+++ cloud_seg/model.py
@@ -1,3 +1,4 @@
+from select import KQ_FILTER_SIGNAL
 import torch 
 import torchvision.transforms.functional as TF 
 import torch.nn as nn 
@@ -9,7 +10,7 @@ class DoubleConv(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
@@ -20,10 +21,32 @@ class DoubleConv(nn.Module):
 
 
 class Unet(nn.Module):
-    def __init__(
-        self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
-    ):
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],):
         super(Unet, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernal_size=2, stride=2)
+
+
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(
+                feature*2, feature, kernel_size=2, stride=2,
+            ))
+
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+    
+
+    
+
+
+
+
+
