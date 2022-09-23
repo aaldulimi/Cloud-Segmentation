beb026fea0b9e37b3c40ce7530bde0d0585be2cd
Ahmed <ahmedamedo22@gmail.com>
2022-09-23 21:16:15 +1000

    added code to train the model

---
+++ cloud_seg/train.py
@@ -0,0 +1,83 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim 
+from model import Unet
+
+
+
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMAGE_DIR = "data/thumbnails/"
+TRAIN_MASK_DIR = "data/masked_png/"
+
+
+def train(loader, model, optimizer, loss_fn, scaler):
+    # one epoch of training 
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward, float16
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm progress
+        loop.set_postfix(loss = loss.item())
+
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,            
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    model = Unet(in_channels=3, out_channels=1).to(device=DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+    train_loader = get_loaders(
+        TRAIN_IMAGE_DIR,
+        TRAIN_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+    )
+    
+    scaler = torch.cuda.amp.grad_scaler()
+    for _ in range(NUM_EPOCHS):
+        train(train_loader, model, optimizer, loss_fn, scaler)
+
+
+if __name__ == "__main__":
+    main()
